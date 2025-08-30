8981cd5f5d9f32af66041ef2a9c40a21d38d7add
Glenn Griggs <glenngr@umich.edu>
2025-08-30 00:18:53 -0400

    Main and init added

--- insta485generator/__main__.py
+++ insta485generator/__main__.py
@@ -1,13 +1,21 @@
 """Build static HTML site from directory of HTML templates and plain files."""
 
 
-def main():
-    """Top level command line interface."""
-    print("Hello world!")
+import click
+
+
+@click.command()
+@click.argument("input_dir", nargs=1, type=click.Path(exists=True))
+def main(input_dir):
+    input_dir = pathlib.Path(input_dir)
+    print(f"DEBUG input_dir={input_dir}")
+
 
 
 if __name__ == "__main__":
     main()
 
 
-    
+
+
+
