1cb1533d9f6d7ad2695b0bdacf6fa7443c91069c
Glenn Griggs <glenngr@umich.edu>
2025-08-30 00:25:05 -0400

    More click changes

--- insta485generator/__main__.py
+++ insta485generator/__main__.py
@@ -1,11 +1,11 @@
 """Build static HTML site from directory of HTML templates and plain files."""
 
-
+import pathlib
 import click
 
 
 @click.command()
-@click.argument("input_dir", nargs=1, type=click.Path(exists=True))
+@click.argument("input_dir", nargs=1 , type=click.Path(exists=True))
 def main(input_dir):
     input_dir = pathlib.Path(input_dir)
     print(f"DEBUG input_dir={input_dir}")
