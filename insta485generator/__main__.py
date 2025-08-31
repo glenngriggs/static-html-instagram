35938fca3f2dec0133a8ac68ed4fcb0c8a48e8f7
Glenn Griggs <glenngr@umich.edu>
2025-08-31 16:32:11 -0400

    Working on click

--- insta485generator/__main__.py
+++ insta485generator/__main__.py
@@ -4,11 +4,27 @@ import pathlib
 import click
 
 
-@click.command()
+@click.command(help="Templated static website generator.")
 @click.argument("input_dir", nargs=1 , type=click.Path(exists=True))
+
+@click.option(
+    "-o", "--output", "output_dir",
+    type=click.Path(file_okay=False, dir_okay=True, writable=True, path_type=pathlib.Path),
+    help="Output directory.",
+)
+@click.option(
+    "-v", "--verbose",
+    is_flag=True,
+    help="Print more output.",
+)
+
+
 def main(input_dir):
     input_dir = pathlib.Path(input_dir)
     print(f"DEBUG input_dir={input_dir}")
+
+
+
 
 
 
